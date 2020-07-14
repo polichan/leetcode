171f7a95b3af793f3e6d26184f49875f49eb04f4
polichan <lazysloths@yahoo.com>
2020-07-14 13:49:38 +0800

    fix: 代码格式化

--- 120.三角形最小路径和/main.go
+++ 120.三角形最小路径和/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	test := [][]int{
 		{2},
@@ -17,18 +15,11 @@ func minimumTotal(triangle [][]int) int {
 	if n == 0 {
 		return 0
 	}
-	//for i := n ; i>= 0 ; i -- {
-	//	for j := len(triangle[i]) - 1; j >= 0; j -- {
-	//		triangle[i][j] = triangle[i][j] + min(triangle[i + 1][j] , triangle[i + 1][j + 1])
-	//	}
-	//}
-
-	for i := 0; i < n  ; i ++ {
-		for j := 0; j < len(triangle[i]) ; j ++ {
+	for i := n ; i>= 0 ; i -- {
+		for j := len(triangle[i]) - 1; j >= 0; j -- {
 			triangle[i][j] = triangle[i][j] + min(triangle[i + 1][j] , triangle[i + 1][j + 1])
 		}
 	}
-	fmt.Println(triangle)
 	return triangle[0][0]
 }
 func min(x, y int)int{
