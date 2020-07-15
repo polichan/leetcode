f4ad479afea00435dd60785d96e38c006f1c303a
Hexwell <lazysloths@yahoo.com>
2020-07-16 00:10:39 +0800

    7. 整数反转

--- 7.整数反转/main.go
+++ 7.整数反转/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "math"
+
 func main() {
-	
+	reverse(123)
+}
+
+
+func reverse(x int) int {
+	ans := 0
+	for x != 0 {
+		if ans > math.MaxInt32 /10 || ans < math.MinInt32 / 10 {
+			return 0
+		}
+		ans = ans * 10
+		ans += x % 10
+		x /= 10
+	}
+	return ans
 }
