f1d381955e4f09be2173e01c164a37ba16d72116
Hexwell <lazysloths@yahoo.com>
2020-07-30 00:23:56 +0800

    198.打家劫舍

---
+++ 198.打家劫舍/main.go
@@ -0,0 +1,29 @@
+package main
+
+func main() {
+	test := []int{1,2,3,1}
+	println(rob(test))
+}
+
+func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+	for i := 2; i < len(nums) ; i++ {
+		dp[i] = max(dp[i - 1 ], dp[i -2 ] + nums[i])
+	}
+	return dp[len(nums) - 1]
+}
+
+func max(x,y int)int  {
+	if x > y {
+		return x
+	}
+	return y
+}
