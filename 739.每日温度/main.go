ba978ae937fb0aa1cad91062a6c73dfbca72911f
Hexwell <lazysloths@yahoo.com>
2020-08-11 01:14:20 +0800

    739.每日温度

--- 739.每日温度/main.go
+++ 739.每日温度/main.go
@@ -1,9 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	t := []int{73, 74, 75, 71, 69, 72, 76, 73}
+	fmt.Println(dailyTemperatures(t))
 }
 
-func dailyTemperatures(T []int) []int {
 
+func dailyTemperatures(T []int) []int {
+	length := len(T)
+	ans := make([]int, length)
+	stack := []int{}
+	for i := 0; i < length; i++ {
+		temperature := T[i]
+		for len(stack) > 0 && temperature > T[stack[len(stack)-1]] {
+			prevIndex := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			ans[prevIndex] = i - prevIndex
+		}
+		stack = append(stack, i)
+	}
+	return ans
 }
