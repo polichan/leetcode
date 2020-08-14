49187d1c4e6be32be91b666782ad93cb67141aad
Hexwell <lazysloths@yahoo.com>
2020-08-14 20:21:56 +0800

    503. 下一个更大的元素

---
+++ 503.下一个更大的元素 II/main.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	
+}
+
+func nextGreaterElements(nums []int) []int {
+	stack := make([]int,0)
+	res := make([]int, len(nums))
+	for i:= 2*len(nums)-1; i>=0 ; i-- {
+		for len(stack) >0 && nums[i%len(nums)]>=nums[stack[len(stack)-1]]{
+			stack = stack[:len(stack)-1]
+		}
+		if len(stack)==0 {
+			res[i%len(nums)] = -1
+		} else {
+			res[i%len(nums)] = nums[stack[len(stack)-1]]
+		}
+		stack = append(stack,i%len(nums))
+	}
+	return res
+}
