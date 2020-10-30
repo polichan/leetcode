2c5e490a3f5122af4a639d9bb8280658ace7d847
Hexwell <pocoa@foxmail.com>
2020-10-30 13:54:52 +0800

    1470. 重新排列数组

--- 1470. 重新排列数组/main.go
+++ 1470. 重新排列数组/main.go
@@ -1,9 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Print(shuffle([]int{1,4,3,2,2,3,4,1}, 4))
 }
 
 func shuffle(nums []int, n int) []int {
-
+	index := 0
+	res := make([]int, len(nums))
+	for i := 0; i < n; i ++ {
+		res[i * 2] = nums[index]
+		res[i * 2 + 1] = nums[index + n]
+		index++
+	}
+	return res
 }
